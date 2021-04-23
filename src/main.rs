15933e1c1035c2ac6389c1d007ff9c579d9fc2cf
Jonas A. Hultén <jonas@sldr.se>
2021-04-23 20:48:20 +0200

    Added rudimentary grabbing of the argv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // Capture the argv
+    let mut argv = env::args();
+    argv.next();
+    let msg_option: Option<String> = argv.next();
+    if msg_option == None {
+        println!("NO COMMIT MESSAGE FILE PASSED");
+    }
+
 }
